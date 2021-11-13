d022c55566a568230742eb09c3b831e9ec831029
Teddy <tomguy8996@gmail.com>
2021-11-14 02:31:43 +0900

    Revert "gradle 변경, Layered Architecture 시범 적용"
    This reverts commit f303fe6b421ce996454f6bf4e62dc2ba4d7a5434.

--- src/main/java/com/guava/judelaw/common/constants/ErrorConstants.java
+++
@@ -1,7 +0,0 @@
-package com.guava.judelaw.common.constants;
-
-public class ErrorConstants {
-    private ErrorConstants() {}
-
-    public static final String LOGIN_FAILED = "login failed";
-}
--- src/main/java/com/guava/judelaw/user/controller/UserController.java
+++ src/main/java/com/guava/judelaw/user/controller/UserController.java
@@ -1,43 +1,17 @@
 package com.guava.judelaw.user.controller;
 
-import com.guava.judelaw.common.constants.ErrorConstants;
-import com.guava.judelaw.common.dto.ErrorDTO;
-import com.guava.judelaw.user.dto.UserDTO;
-import com.guava.judelaw.user.model.UserEntity;
-import com.guava.judelaw.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RestController
 public class UserController {
 
-    private UserService userService;
+    @GetMapping("/user")
+    public void getUserInfo() {
 
-    @PostMapping("/signin")
-    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
-        UserEntity user = userService.getByCredentials(
-                userDTO.getUserId(),
-                userDTO.getPassword()
-        );
-
-        if (user == null) {
-            ErrorDTO errorDTO = ErrorDTO.builder().message(ErrorConstants.LOGIN_FAILED).build();
-            return ResponseEntity.badRequest().body(errorDTO);
-        }
-
-        final UserDTO responseUserDTO = UserDTO.builder()
-                .userId(user.getUserId())
-                .licenseNo(user.getLicenseNo())
-                .userName(user.getUserName())
-                .email(user.getEmail())
-                .mobile(user.getMobile())
-                .build();
-
-        return ResponseEntity.ok().body(responseUserDTO);
     }
 }
